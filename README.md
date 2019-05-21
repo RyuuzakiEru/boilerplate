f1db130d5cdeebcaafb246c8c629fe8900ece359
Ryuuzaki <rmorales82@outlook.com>
2019-05-21 08:16:15 +0200

    Waves Keeper Auth integrated

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Demo mastering web3 with waves app
+https://web3-boilerplate.herokuapp.com/
--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,22 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   state = {};
+  authFunc = () => {
+    const authData = { data: "Auth on my site" };
+    if (WavesKeeper) {
+      WavesKeeper.auth(authData)
+        .then(auth => {
+          console.log(auth); //displaying the result on the console
+          /*...processing data */
+        })
+        .catch(error => {
+          console.error(error); // displaying the result on the console
+          /*...processing errors */
+        });
+    } else {
+      alert("To Auth WavesKeeper should be installed.");
+    }
+  };
 
   render() {
     return (
@@ -10,9 +26,7 @@ export default class App extends Component {
           className="btn btn-primary"
           type="submit"
           value="Alert"
-          onClick={() => {
-            alert("Alert button onClick");
-          }}
+          onClick={this.authFunc}
         />
       </div>
     );
